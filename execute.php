f6450f7e20254a048e95e3d53a637dc9dc1fd96f
Enricocalonico18 <45965248+Enricocalonico18@users.noreply.github.com>
2018-12-18 11:52:42 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -46,7 +46,7 @@
 		//i parametri sono cosa voglio mandare indietro al mio utente, rimando il testo che
 		//ho ricevuto e che si trova nella variabile $text
 		$parameters = array('chat_id' => $chatId, "text" => $text);
-		If($text='data'){
+		If($text=='data'){
 		$text="la data odierna è:".date("d.m.a")
 		$parameters=array('chat_id' =>$chatId, "text" =>$text);
 		}
